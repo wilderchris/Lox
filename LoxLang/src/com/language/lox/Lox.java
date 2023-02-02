c68e8dcbfdda263356a27cd50fcef2caff3d4e96
wilderchris <94183821+wilderchris@users.noreply.github.com>
2023-02-02 16:42:34 -0600

    add scanner

--- LoxLang/src/com/language/lox/Lox.java
+++ LoxLang/src/com/language/lox/Lox.java
@@ -33,7 +33,6 @@ public class Lox {
 	private static void run(String source) {
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens = scanner.scanTokens();
-
 		for (Token token : tokens) {
 			System.out.println(token);
 		}

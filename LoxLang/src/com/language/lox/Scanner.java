30a981d8575f37ece47b00b553333810f127b4da
wilderchris <94183821+wilderchris@users.noreply.github.com>
2023-02-03 14:58:12 -0600

    Update Scanner.java

--- LoxLang/src/com/language/lox/Scanner.java
+++ LoxLang/src/com/language/lox/Scanner.java
@@ -70,10 +70,30 @@ private int line = 0;
 	        case '\n':
 	          line++;
 	          break;
+	        case '"': string(); break;
 	      default:
 	          Lox.error(line, "Unexpected character.");
 	          break;
 	    }
+	  }
+	
+	private void string() {
+	    while (peek() != '"' && !isAtEnd()) {
+	      if (peek() == '\n') line++;
+	      advance();
+	    }
+
+	    if (isAtEnd()) {
+	      Lox.error(line, "Unterminated string.");
+	      return;
+	    }
+
+	    // The closing ".
+	    advance();
+
+	    // Trim the surrounding quotes.
+	    String value = source.substring(start + 1, current - 1);
+	    addToken(STRING, value);
 	  }
 	
 	private boolean match(char expected) {
